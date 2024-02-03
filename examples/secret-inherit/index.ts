14245609181c63b0491f40f95b3a73da1038f53a
Leechael Yim <yanleech@gmail.com>
2024-02-03 18:04:40 +0800

    docs: example for secret-inherit

---
+++ examples/secret-inherit/index.ts
@@ -0,0 +1,21 @@
+interface SerizliedRequest {
+  method: 'GET' | 'POST' | 'PATCH' | 'PUT';
+  path: string;
+  headers: Record<string, string>;
+  body: string;
+  secret?: Record<string, unknown>;
+}
+
+export default async function main(payload: string) {
+  const request: SerizliedRequest = JSON.parse(payload);
+  const answer = request.secret?.answer ?? '42'
+  const name = request.secret?.name ?? 'World'
+  return JSON.stringify({
+    headers: {
+      'Content-Type': 'text/plain',
+      'Cache-Control': 'public, max-age: 60',
+      'Expires': new Date(Date.now() + 60 * 1000).toUTCString(),
+    },
+    body: `Hi ${name}, the answer is ${answer}!`,
+  })
+}
