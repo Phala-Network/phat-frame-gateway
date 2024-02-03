9ced5dd9aeffcf35d002632ea62a0b41a6d55a84
Leechael Yim <yanleech@gmail.com>
2024-02-03 15:16:09 +0800

    imp: close redis connection on memoize end

--- src/memoize.ts
+++ src/memoize.ts
@@ -25,66 +25,73 @@ export function memoize<TResult = unknown, TArgs extends Array<any> = any[]>(
       redis = new Redis(process.env.MEMOIZE_REDIS_URI)
     }
 
-    for (let adapter of adapters) {
-      switch (adapter) {
-        case 'memory':
-          if (cache.has(key)) {
-            console.log(`cache hit: ${key}`)
-            return cache.get(key)
-          }
-          break
-
-        case 'redis':
-          if (!redis) {
-            throw new Error('redis adapter requires setup environment variable MEMOIZE_REDIS_URI first.')
-          }
-          const result = await redis.get(key)
-          if (result) {
-            console.log(`cache hit: ${key}`)
-            return JSON.parse(result)
-          }
-          break
-
-        case 'fs':
-          const restrictPrefix = fs.realpathSync('./storage')
-          const unsafePath = `./storage/${key}`
-          if (!fs.existsSync(unsafePath)) {
+    try {
+      for (let adapter of adapters) {
+        switch (adapter) {
+          case 'memory':
+            if (cache.has(key)) {
+              console.log(`cache hit: ${key}`)
+              return cache.get(key)
+            }
             break
-          }
-          const fullpath = fs.realpathSync(unsafePath)
-          if (!fullpath.startsWith(restrictPrefix)) {
-            console.log(fullpath)
-            console.log(unsafePath)
-            throw new Error(`malformed key: ${key}`)
-          }
-          console.log(`cache hit: ${key}`)
-          return fs.readFileSync(fullpath, 'utf-8')
+
+          case 'redis':
+            if (!redis) {
+              throw new Error('redis adapter requires setup environment variable MEMOIZE_REDIS_URI first.')
+            }
+            const result = await redis.get(key)
+            if (result) {
+              console.log(`cache hit: ${key}`)
+              return JSON.parse(result)
+            }
+            break
+
+          case 'fs':
+            const restrictPrefix = fs.realpathSync('./storage')
+            const unsafePath = `./storage/${key}`
+            if (!fs.existsSync(unsafePath)) {
+              break
+            }
+            const fullpath = fs.realpathSync(unsafePath)
+            if (!fullpath.startsWith(restrictPrefix)) {
+              console.log(fullpath)
+              console.log(unsafePath)
+              throw new Error(`malformed key: ${key}`)
+            }
+            console.log(`cache hit: ${key}`)
+            return fs.readFileSync(fullpath, 'utf-8')
+        }
+      }
+
+      console.log(`cache miss: ${key}`)
+      const result = await asyncFn.apply(null, args)
+
+      for (let adapter of adapters) {
+        switch (adapter) {
+          case 'memory':
+            cache.set(key, result)
+            break
+
+          case 'redis':
+            if (!redis) {
+              throw new Error('redis adapter requires setup environment variable MEMOIZE_REDIS_URI first.')
+            }
+            await redis.set(key, JSON.stringify(result), 'EX', ttl)
+            break
+
+          case 'fs':
+            fs.writeFileSync(`./storage/${key}`, JSON.stringify(result))
+            break
+        }
+      }
+
+      return result
+    } finally {
+      if (redis) {
+        redis.disconnect()
+        redis.quit()
       }
     }
-
-    console.log(`cache miss: ${key}`)
-    const result = await asyncFn.apply(null, args)
-
-    for (let adapter of adapters) {
-      switch (adapter) {
-        case 'memory':
-          cache.set(key, result)
-          break
-
-        case 'redis':
-          if (!redis) {
-            throw new Error('redis adapter requires setup environment variable MEMOIZE_REDIS_URI first.')
-          }
-          await redis.set(key, JSON.stringify(result), 'EX', ttl)
-          break
-
-        case 'fs':
-          fs.writeFileSync(`./storage/${key}`, JSON.stringify(result))
-          break
-      }
-    }
-
-    return result
   }
 }
 
